f8ed99abe9427f5a0414ce6ea835f696e25b4340
Eugen Krieger <eugenkrieger@gmail.com>
2015-04-04 19:18:49 -0700

    euk week 1: solved 1.1 -1.3 ready to review

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,39 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(col: Int, row: Int): Int = 
+      if (row == 0 || col == 0 || col == row) 1
+      else if (col < 0 || col > row) 0
+      else pascal(col, row - 1) + pascal(col - 1, row - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(word: List[Char]): Boolean = {
+    var w = word.filter(x => x == ')' || x == '(')
+    if (w.head.equals(')') || w.last.equals('(')) false
+
+    def innerbalance(c: Int, cword: List[Char]): Boolean = {
+      if (c < 0) false
+      else if (cword.isEmpty) c == 0
+      else if (cword.head.equals('(')) innerbalance(c + 1, cword.tail)
+      else innerbalance(c - 1, cword.tail)
+    }
+    innerbalance(0, w)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int =
+    {
+      def countChangeInner(m: Int, c: List[Int]): Int =
+        {
+          if(m < 0 || c.isEmpty ) 0
+          else if (m == 0) 1
+          else countChangeInner(m - c.head, c) + countChangeInner(m, c.tail)
+        }
+      if(money < 1) 0
+      else countChangeInner(money, coins)
+    }
 }
